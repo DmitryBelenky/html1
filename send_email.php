5dbb615e5d5d204fd00e6ee5d36f59ff6f1f4df8
DmitryBelenky <134544839+DmitryBelenky@users.noreply.github.com>
2023-05-30 18:37:08 +0300

    Add files via upload

--- send_email.php
+++ send_email.php
@@ -1,34 +1,16 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = test_input($_POST["name"]);
-  $email = test_input($_POST["email"]);
-  $message = test_input($_POST["message"]);
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "dmitry.blb5@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: my website" . "\r\n" .
 
-  // Validate email
-  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    die("Invalid email format");
-  }
-
-  // Email content
-  $to = "dmitry.bl85@gmail.com";
-  $headers = "From: $email" . "\r\n" . "Reply-To: $email" . "\r\n";
-  $email_content = "Name: $name\n";
-  $email_content .= "Email: $email\n";
-  $email_content .= "Message: $message\n";
-
-  // Send email
-  if (mail($to, $email_content, $headers)) {
-    header("Location: thank_you.html");
-    exit;
-  } else {
-    die("Failed to send email");
-  }
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
 }
-
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
+//redirect
+header("Location:thank_you.html");
 ?>
